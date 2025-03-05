0a375997eed6591c58c91ba07e4c1c957e9a91f2
Rudnev Konstantin <konstrudserg.030408@gmail.com>
2025-03-05 22:08:16 +0300

    Шаблоны: Задание 2

--- lesson_2/server.py
+++ lesson_2/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 
 app = Flask(__name__)
@@ -13,6 +14,20 @@ def index(title):
         title=title
         )
 
+@app.route("/training/<prof>")
+def training(prof):
+    if "инженер" in prof or "строитель" in prof:
+        prof_type = "engineering"
+    else:
+        prof_type = "ology"
+    
+    return render_template(
+        "training.html",
+        title="training",
+        image_path=url_for("static", filename="MARS-2-2.png"),
+        prof=prof_type
+    )
+
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080)
+    app.run(host="127.0.0.1", port=8080, debug=True)
